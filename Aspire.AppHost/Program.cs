7f9b19238034e2d589f6ef299dbc63ac5273c74a
jackson <jacksonmdev@outlook.com>
2025-01-26 22:28:22 +0800

    chore: added postgresql to aspire

--- Aspire.AppHost/Program.cs
+++ Aspire.AppHost/Program.cs
@@ -1,5 +1,11 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-builder.AddProject<Projects.Web>("web-api");
+var database = builder.AddPostgres("app-db")
+    .WithDataVolume()
+    .WithPgAdmin();
+
+builder.AddProject<Projects.Web>("app-api")
+    .WithReference(database)
+    .WaitFor(database);
 
 builder.Build().Run();
