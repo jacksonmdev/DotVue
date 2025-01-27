f321ef6235d325c2e64da3cddf1862858fb3079d
jackson <jacksonmdev@outlook.com>
2025-01-27 08:13:55 +0800

    fix: vue setup

--- Aspire.AppHost/Program.cs
+++ Aspire.AppHost/Program.cs
@@ -4,8 +4,15 @@ var database = builder.AddPostgres("app-db")
     .WithDataVolume()
     .WithPgAdmin();
 
-builder.AddProject<Projects.Web>("app-api")
+var api = builder.AddProject<Projects.Web>("app-api")
     .WithReference(database)
     .WaitFor(database);
+
+builder.AddNpmApp("app-vue", "../Web/VueApp")
+    .WithReference(api)
+    .WaitFor(api)
+    //.WithHttpEndpoint(env: "8080")
+    //.WithExternalHttpEndpoints()
+    .PublishAsDockerFile();
 
 builder.Build().Run();
