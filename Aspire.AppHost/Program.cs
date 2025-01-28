77c40ab5ff9f286d89de12b02835c2ab6c24bcc5
jackson <jacksonmdev@outlook.com>
2025-01-28 11:27:03 +0800

    chore: updated settings

--- Aspire.AppHost/Program.cs
+++ Aspire.AppHost/Program.cs
@@ -12,6 +12,7 @@ var api = builder.AddProject<Projects.Web>("app-api")
 builder.AddNpmApp("app-vue", "../Web/VueApp")
     .WithReference(api)
     .WaitFor(api)
+    .WithEnvironment("VITE_API_BASE_URL", "https://localhost:7000")
     .WithHttpEndpoint(targetPort: 4000, port: 8080)
     .WithExternalHttpEndpoints()
     .PublishAsDockerFile();
