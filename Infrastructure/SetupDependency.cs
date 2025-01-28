44b7f5114afbd37880b55a907d9e763b8c2bd254
jackson <jacksonmdev@outlook.com>
2025-01-28 21:05:43 +0800

    chore: initial db context setup

--- Infrastructure/SetupDependency.cs
+++ Infrastructure/SetupDependency.cs
@@ -1,4 +1,9 @@
-﻿using Microsoft.Extensions.Hosting;
+﻿using Application.Common.Interfaces;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Infrastructure;
 
@@ -6,6 +11,15 @@ public static class SetupDependency
 {
     public static void AddInfrastructureServices(this IHostApplicationBuilder builder)
     {
-        
+        var connectionString = builder.Configuration.GetConnectionString("DotVueDb");
+
+        builder.Services.AddDbContext<ApplicationDbContext>((sp, options) =>
+        {
+            // options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+            options.UseNpgsql(connectionString);
+        });
+
+        builder.Services.AddScoped<IApplicationDbContext>(provider =>
+            provider.GetRequiredService<ApplicationDbContext>());
     }
 }
