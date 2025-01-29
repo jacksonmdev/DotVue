67daf52651053c623a1ac0adfb3450f9e81f0a84
jackson <jackson@asus-rog-strix>
2025-01-29 20:47:21 +0800

    chore: Mapping setup

---
+++ Application/Common/Models/ForecastDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Application.Common.Models;
+
+public class ForecastDto
+{
+    private class Mapping : Profile
+    {
+        public string? Summary { get; set; }
+        public int TemperatureC { get; set; }
+        
+        public Mapping()
+        {
+            CreateMap<WeatherForecast, ForecastDto>();
+        }
+    }
+}
