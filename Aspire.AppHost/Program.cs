4e510ffcb07d95b0dd1e78745bfd35aaaa16252b
Jackson Mangallay <jacksonmdev@outlook.com>
2025-02-16 13:12:32 +0800

    fix?: aspire db conn (#6)
    Co-authored-by: jackson <jackson@asus-rog-strix>

--- Aspire.AppHost/Program.cs
+++ Aspire.AppHost/Program.cs
@@ -4,16 +4,11 @@ var database = builder.AddPostgres("app-db")
     .WithDataVolume()
     .WithPgAdmin()
     .AddDatabase("DotVueDB");
-    // .WithEnvironment("POSTGRES_USER", "postgres") // Set the PostgreSQL user
-    // .WithEnvironment("POSTGRES_PASSWORD", "slump6Fluent6sal!va") // Set the PostgreSQL password
-    // .WithEnvironment("POSTGRES_DB", "postgres");
-    // .WithHttpEndpoint(targetPort: 5432, port: 5400)
-    // .WithExternalHttpEndpoints();
 
 var api = builder.AddProject<Projects.Web>("app-api")
     .WithExternalHttpEndpoints()
     .WithReference(database)
-    // .WithEnvironment("dotnetvue_db", "Host=app-db;Database=dotvue_db;Username=postgres;Password=h5mAwV3SAsdAFyac;Include Error Detail=true;")
+    .WithEnvironment("ConnectionStrings__DotVueDb", "Host=localhost;Database=dotvue_db;Username=postgres;Password=h5mAwV3SAsdAFyac;Include Error Detail=true;") //Comment this out to use DB inside Aspire
     .WaitFor(database);
 
 builder.AddNpmApp("app-vue", "../Web/VueApp")
