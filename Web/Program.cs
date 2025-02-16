3d3ead9ab540d297bb0066f0cdba9f81c05624ea
jackson <jackson@asus-rog-strix>
2025-02-16 19:26:18 +0800

    fix: azure app config

--- Web/Program.cs
+++ Web/Program.cs
@@ -7,17 +7,21 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Setup Azure App Configuration
 var endpoint = builder.Configuration.GetConnectionString("AppConfiguration");
-builder.Configuration.AddAzureAppConfiguration(options =>
+
+if (!string.IsNullOrEmpty(endpoint))
 {
-    options
-        .Connect(endpoint)
-        .ConfigureRefresh(refreshOptions =>
-        {
-            refreshOptions.Register("Sentinel", refreshAll: true);
-        })
-        .Select(KeyFilter.Any)
-        .Select(KeyFilter.Any,  builder.Environment.EnvironmentName);
-});
+    builder.Configuration.AddAzureAppConfiguration(options =>
+    {
+        options
+            .Connect(endpoint)
+            .ConfigureRefresh(refreshOptions =>
+            {
+                refreshOptions.Register("Sentinel", refreshAll: true);
+            })
+            .Select(KeyFilter.Any)
+            .Select(KeyFilter.Any,  builder.Environment.EnvironmentName);
+    });   
+}
 
 // Strictly Enforce int Type on payloads
 builder.Services.ConfigureHttpJsonOptions(options =>
