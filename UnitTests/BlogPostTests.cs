b548de71ef7e54d7220a65dbaaf8a226739f8de7
jacksonm <jackson.mangallay@supportteam.com.au>
2025-02-20 14:53:01 +0800

    chore: sample unit test

---
+++ UnitTests/BlogPostTests.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Entities;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace UnitTests;
+
+public class BlogPostTests
+{
+    [Test]
+    public void ShouldAppendBlogPost() 
+    {
+        var blogPosts = new List<BlogPost>();
+        var blog = new BlogPost
+        {
+            Title = "Hello, Jane Doe",
+            PostId = new Guid(),
+            Status = Domain.Enums.PostStatus.Published
+        };
+
+        blogPosts.Add(blog);
+
+        blogPosts.Count.Should().BeGreaterThan(0);
+    }
+}
