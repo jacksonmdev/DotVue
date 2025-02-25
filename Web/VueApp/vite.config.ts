b533b08858acdab8811a8bcb2c3f5267e98af04c
jacksonm <jackson.mangallay@supportteam.com.au>
2025-02-25 21:45:06 +0800

    chore: added pwa

--- Web/VueApp/vite.config.ts
+++ Web/VueApp/vite.config.ts
@@ -2,6 +2,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -15,6 +16,12 @@ export default defineConfig({
     quasar({
       sassVariables: 'src/assets/quasar-variables.scss',
     }),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true
+      }
+    })
   ],
   resolve: {
     alias: {
